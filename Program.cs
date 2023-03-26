f5b4b4c3b2cc2e3f80ab35ca3334c0f1e44f789c
ganbar.guluyev <ganbarguluyev@gmail.com>
2023-03-26 18:30:02 +0300

    feat: @user input

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi");
+
+            Console.WriteLine("Birinci Sayıyı Giriniz: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("İkinci Sayıyı Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
         }
     }
 }
