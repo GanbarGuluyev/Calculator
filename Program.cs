488b0f6359ac9cb2b03d1ddfaa9677035bb1dcf1
ganbar.guluyev <ganbarguluyev@gmail.com>
2023-03-26 18:33:37 +0300

    fix: add forgotten input

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@
             Console.WriteLine("Birinci Sayıyı Giriniz: ");
             int sayi1 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("İşlem Seçiniz (+) (-) (*) (/) (%): ");
+            int islem = int.Parse(Console.ReadLine());
+
             Console.WriteLine("İkinci Sayıyı Giriniz: ");
             int sayi = int.Parse(Console.ReadLine());
 
