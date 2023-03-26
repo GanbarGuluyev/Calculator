7f70edb5b5b299666c2daa006d601ca627d53efb
ganbar.guluyev1 <ganbarguluyev@gmail.com>
2023-03-26 20:42:32 +0300

    test: df

--- Program.cs
+++ Program.cs
@@ -12,30 +12,20 @@
             Console.WriteLine("İşlem Seçiniz (+) (-) (*) (/) (%): ");
             string islem = Console.ReadLine();
 
-            Console.WriteLine("İkinci Sayıyı Giriniz: ");
-            double sayi = double.Parse(Console.ReadLine());
+            Console.WriteLine("İkinci Sayıyı  Giriniz: ");
+            double sayi2 = double.Parse(Console.ReadLine());
 
             double sonuc = 0;
             switch (islem)
             {
-                case "+":
-                    sonuc = sayi1 + sayi;
-                    break;
-                case "-":
-                    sonuc = sayi1 - sayi;
-                    break;
-                case "*":
-                    sonuc = sayi1 * sayi;
-                    break;
-                case "/":
-                    sonuc = sayi1 / sayi;
-                    break;
-                case "%":
-                    sonuc = sayi1 % sayi;
-                    break;
+                case "+": sonuc = sayi1 + sayi2; break;
+                case "-": sonuc = sayi1 - sayi2; break;
+                case "*": sonuc = sayi1 * sayi2; break;
+                case "/": sonuc = sayi1 / sayi2; break;
+                case "%": sonuc = sayi1 % sayi2; break;
             }
-            Console.WriteLine(sayi1 + " " + islem + " " + sayi + " = " + sonuc);
 
+            Console.WriteLine(sayi1 + " " + islem + " " + sayi2 + " = " + sonuc);
         }
     }
 }
